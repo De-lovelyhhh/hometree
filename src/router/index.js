c0d3d57ab75ca9d06394bac5a87b28e2ac1d8d3b
Hannah <1024631045@qq.com>
2019-06-12 11:26:50 +0800

    打包部署

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import SignPage from '@/components/SignPage.vue'
+import ProblemFind from '@/components/ProblemFind.vue'
+import AfterLogin from '@/components/AfterLogin.vue'
+import Check from '@/components/Check.vue'
+import Notice from '@/components/Notice.vue'
+import Tree from '@/components/Tree.vue'
+import Personal from '@/components/Personal.vue'
+import EditInformation from '@/components/EditInformation.vue'
+import AreaPicker from '@/components/AreaPicker.vue'
 
 Vue.use(Router)
 
@@ -12,6 +21,51 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/SignPage',
+      name: 'SignPage',
+      component: SignPage
+    },
+    {
+      path: '/ProblemFind',
+      name: 'ProblemFind',
+      component: ProblemFind
+    },
+    {
+      path: '/AfterLogin',
+      name: 'AfterLogin',
+      component: AfterLogin
+    },
+    {
+      path: '/Check',
+      name: 'Check',
+      component: Check
+    },
+    {
+      path: '/Notice',
+      name: 'Notice',
+      component: Notice
+    },
+    {
+      path: '/Tree',
+      name: 'Tree',
+      component: Tree
+    },
+    {
+      path: '/Personal',
+      name: 'Personal',
+      component: Personal
+    },
+    {
+      path: '/EditInformation',
+      name: 'EditInformation',
+      component: EditInformation
+    },
+    {
+      path: '/AreaPicker',
+      name: 'AreaPicker',
+      component: AreaPicker
     }
   ]
 })
